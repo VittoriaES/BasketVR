4e6d94738d29d796d75a45cc3cd7aeb5b2df13b6
Fred Vassart <fredvassart@gmail.com>
2021-09-08 11:15:26 +0200

    add test scene and assets

---
+++ Assets/Projects/Scripts/ScoreKeeping.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeping : MonoBehaviour
+{
+    private int score = O
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
