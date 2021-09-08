e6039c73e09d5cfa629e2901978e67e5345e9139
Fred Vassart <fredvassart@gmail.com>
2021-09-08 13:48:50 +0200

    added teleport area

--- Assets/Projects/Scripts/ScoreKeeping.cs
+++ Assets/Projects/Scripts/ScoreKeeping.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreKeeping : MonoBehaviour
 {
+    public TextMeshProUGUI currentScore;
     private int score = 0;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,7 @@ public class ScoreKeeping : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentScore.text = (currentScore.ToString());
     }
 
     private void OnTriggerEnter(Collider other) {
